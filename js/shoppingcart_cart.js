c6c15c3540f7246c9a04ab8c3c7889c771e1e6c2
Flavio Copes <copesc@gmail.com>
2017-03-31 12:35:55 +0200

    Fix [#52](https://github.com/flaviocopes/grav-plugin-shoppingcart/issues/52) add subtotal label

--- js/shoppingcart_cart.js
+++ js/shoppingcart_cart.js
@@ -517,7 +517,7 @@
         if (ShoppingCart.currentPageIsProduct) {
             row += '<td class="goback"><a href="#" class="btn btn-success js__shoppingcart__continue-shopping">' + window.PLUGIN_SHOPPINGCART.translations.CONTINUE_SHOPPING + '</a></td>';
         } else {
-            row += '<td class="empty"></td>';
+            row += '<td class="empty"><strong>' + window.PLUGIN_SHOPPINGCART.translations.SUBTOTAL + '</strong></td>';
         }
 
         row += '<td class="empty"></td>';
